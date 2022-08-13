9bbadc258a616c9042b0e8c977c947487428314f
Aleksandr Gorshkov <al.gorshkoov@gmail.com>
2022-08-13 07:00:43 +0000

    Add eth_sign_typed_data_v4 (#51)
    * Add wallet_switchEthereumChain method
    
    * Add eth_signTypedData_v4 method

--- walletconnect/src/main/java/com/trustwallet/walletconnect/models/WCMethod.kt
+++ walletconnect/src/main/java/com/trustwallet/walletconnect/models/WCMethod.kt
@@ -18,6 +18,9 @@ enum class WCMethod {
     @SerializedName("eth_signTypedData")
     ETH_SIGN_TYPE_DATA,
 
+    @SerializedName("eth_signTypedData_v4")
+    ETH_SIGN_TYPE_DATA_V4,
+
     @SerializedName("eth_signTransaction")
     ETH_SIGN_TRANSACTION,
 
